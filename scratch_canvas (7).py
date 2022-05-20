2c8bb24ba7ce7ddf51a806feb15b526414b510d2
DummyCoder1 <100186733+DummyCoder1@users.noreply.github.com>
2022-05-20 08:34:54 +0530

    Add files via upload

---
+++ scratch_canvas (7).py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+temperature =[17,20,24,30,32,35,40]
+precipation =[300,200,200,250,240,220,330]
+plt.xlabel("Temperature in degree c")
+plt.ylabel("precipation in centimeters")
+plt.scatter(temperature,precipation)
+plt.show()
+
